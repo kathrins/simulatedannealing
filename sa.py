a07775b389aaf9fb5b630ef520bb80a0077063ae
kathrins <38502186+kathrins@users.noreply.github.com>
2018-07-15 11:40:10 +0200

    inital solution

--- sa.py
+++ sa.py
@@ -2,7 +2,7 @@ import scipy.stats as sst
 import numpy as np
 import scipy.spatial.distance as ssp
 import matplotlib.pyplot as plt
-import itertools as iter
+import itertools
 
 # SIMULATED ANNEALING
 
@@ -11,7 +11,8 @@ data = np.genfromtxt('./data/Borden3DMegaInfo.txt',
                      skip_header=1)
 # select data from plane with z=1
 # data columns: X,Y,Z,K
-data2d = data[data[:,2] == 1,:]
+z=1
+data2d = data[data[:,2] == z,:]
 X=data2d[:,0]
 Y=data2d[:,1]
 # -------------------------------------------
@@ -48,13 +49,13 @@ def calc_variogram(dataset):
     return vario
 
 
-<<<<<<< HEAD
-def build_edf(measurements,):
-=======
+#<<<<<<< HEAD
+#def build_edf(measurements,):
+#=======
 
 
 def build_edf(measurements):
->>>>>>> d3d835bb6999f864c18e50bd689673ac6f6e8f4d
+#>>>>>>> d3d835bb6999f864c18e50bd689673ac6f6e8f4d
     """builds an edf of a 1D np array"""
     npF1 = np.array(measurements)
     nPts = np.shape(measurements)[0]
@@ -72,24 +73,24 @@ def update_vario (sol,solnew, d,vario,r1,r2):
     for i in range (bins.shape[0]):
         d_r1=d[:,r1]
         idx1=np.where(np.logical_and(d_r1>bins[i],d_r1<=bins[i+1]))
-        var_old = np.zeros(len(idx[0]),)
-        var_new = np.zeros(len(idx[0]),)
+        var_old = np.zeros(len(idx1[0]),)
+        var_new = np.zeros(len(idx1[0]),)
         if np.sum(idx1) > 0:
             for j in range (len(idx1)):
-                var_old[j] = np.square(sol[[idx[0][j]],3] - sol[[idx[1][j]],3])
-                var_new[j] = np.square(solnew[[idx[0][j]],3] - solnew[[idx[1][j]],3])
+                var_old[j] = np.square(sol[[idx1[0][j]],3] - sol[[idx1[1][j]],3])
+                var_new[j] = np.square(solnew[[idx1[0][j]],3] - solnew[[idx1[1][j]],3])
             
             vario_new[i,1]=vario[i,1]-np.sum(var_old)+np.sum(var_new)
         
     for i in range (bins.shape[0]):
         d_r2=d[:,r2]
         idx1=np.where(np.logical_and(d_r2>bins[i],d_r2<=bins[i+1]))
-        var_old = np.zeros(len(idx[0]),)
-        var_new = np.zeros(len(idx[0]),)
+        var_old = np.zeros(len(idx1[0]),)
+        var_new = np.zeros(len(idx1[0]),)
         if np.sum(idx1) > 0:
             for j in range (len(idx1)):
-                var_old[j] = np.square(sol[[idx[0][j]],3] - sol[[idx[1][j]],3])
-                var_new[j] = np.square(solnew[[idx[0][j]],3] - solnew[[idx[1][j]],3])
+                var_old[j] = np.square(sol[[idx1[0][j]],3] - sol[[idx1[1][j]],3])
+                var_new[j] = np.square(solnew[[idx1[0][j]],3] - solnew[[idx1[1][j]],3])
             
     vario_new[i,1]=vario[i,1]-np.sum(var_old)+np.sum(var_new)
         
@@ -101,28 +102,21 @@ def update_vario (sol,solnew, d,vario,r1,r2):
 # Select a stopping criterion
 
 #initial solution
-<<<<<<< HEAD
-randK=np.random.normal(0,1,data2d.shape[0])
-sol=data2d
-sol[:,3]=randK
-=======
+#<<<<<<< HEAD
+#randK=np.random.normal(0,1,data2d.shape[0])
+#sol=data2d
+#sol[:,3]=randK
+#=======
 x=np.linspace(np.min(X),np.max(X),99)
 y=np.linspace(np.min(Y),np.max(Y),99)
-xy=np.zeros(9999,1)
-for i in range (99):
-    for j in range (99):
-        xy[i,0]
-xy=list(iter.product(x,y))
-z=np.ones(x.shape[0])
-
-randK=np.random.normal(0,1,x.shape[0])
-xy.append(z)
-xy.append(randK)
-sol = np.array(xy)
+xy=np.array(list(itertools.product(x,y)))
+zz=np.zeros(xy.shape[0])+z
+randK=np.random.normal(0,1,xy.shape[0])
+sol = np.concatenate((xy,zz.reshape(zz.shape[0],1),randK.reshape(randK.shape[0],1)),axis=1)
 
 d,d1,d2 = calc_distmatrix(sol)
 bins = create_bins(d)
->>>>>>> d3d835bb6999f864c18e50bd689673ac6f6e8f4d
+#>>>>>>> d3d835bb6999f864c18e50bd689673ac6f6e8f4d
 
 #annealing schedule from Deutsch and Cockerham, Table 1 Default
 # (t0, lambda, K max, K accept, S, O min)
@@ -165,10 +159,10 @@ while it<100:
 
 
     # Calculate Objective Function f(j) UPDATE
-<<<<<<< HEAD
+#<<<<<<< HEAD
     f=np.mean(np.square(f0-calc_variogram(solnew)))
 
-=======
+#=======
     #f=np.mean(np.square(f0-calc_variogram(solnew)))
     #f=update_vario(sol,solnew, d,vario,r1,r2)
 
@@ -200,7 +194,7 @@ while it<100:
         vario_new[i,1]=vario[i,1]-np.sum(var_old)+np.sum(var_new)
 
     f=np.mean(np.square(vario_new-vario))
->>>>>>> d3d835bb6999f864c18e50bd689673ac6f6e8f4d
+#>>>>>>> d3d835bb6999f864c18e50bd689673ac6f6e8f4d
     delta = f-fold
     
     # if delta = f(j)-f(i) < 0:
